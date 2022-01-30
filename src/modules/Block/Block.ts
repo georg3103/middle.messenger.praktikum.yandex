bc8ed4c7a97fe0adcbc2667b668d9ead3abfff4c
mkrtchyan_gn <georgiy.mkrtchyan@open.ru>
2022-01-30 17:41:06 +0300

    feat(modules): add Block + EventBus

---
+++ src/modules/Block/Block.ts
@@ -0,0 +1,186 @@
+import {v4 as makeUUID} from 'uuid';
+import EventBus from '../EventBus';
+
+const componentAttr = 'data-component';
+
+interface Props {
+  components?: Record<string, Block>;
+  events?: Record<string, EventListenerObject>
+};
+
+export default class Block {
+  static EVENTS = {
+    INIT: 'init',
+		FLOW_CDM: 'flow:component-did-mount',
+		FLOW_CDU: 'flow:component-did-update',
+		FLOW_RENDER: 'flow:render',
+  };
+
+  eventBus: EventBus;
+  _element: HTMLElement;
+  _meta: {
+    tagName: string;
+    props: Props;
+  };
+  _id: string;
+  props: Props;
+
+  constructor(tagName: string, props: Props) {
+    this._meta = {
+      tagName,
+      props,
+    };
+
+    this._id = makeUUID();
+    this.props = this._makePropsProxy(props);
+
+    this.eventBus = new EventBus();
+
+    this._registerEvents(this.eventBus);
+    this.eventBus.emit(Block.EVENTS.INIT);
+  }
+
+  _registerEvents(eventBus: EventBus) {
+		eventBus.on(Block.EVENTS.INIT, this.init.bind(this));
+		eventBus.on(Block.EVENTS.FLOW_CDM, this._componentDidMount.bind(this));
+		eventBus.on(Block.EVENTS.FLOW_CDU, this._componentDidUpdate.bind(this));
+		eventBus.on(Block.EVENTS.FLOW_RENDER, this._render.bind(this));
+	}
+
+  _componentDidUpdate<T>(oldProps: T, newProps: T): void {
+    const hasPropsChanged = this.isPropsChanged(oldProps, newProps);
+    if (!hasPropsChanged) {
+      return;
+    }
+
+    this._render();
+  }
+
+  _componentDidMount(): void {
+		this.componentDidMount();
+		this.eventBus.emit(Block.EVENTS.FLOW_RENDER);
+	}
+
+  componentDidMount(): void {}
+
+  init(): void {
+		this._createResources();
+		this.eventBus.emit(Block.EVENTS.FLOW_CDM);
+	}
+
+  _createResources() {
+    const { tagName } = this._meta;
+		this._element = this._createDocumentElement(tagName);
+  }
+
+  _createDocumentElement(tagName: string): HTMLElement {
+		const element = document.createElement(tagName);
+		element.setAttribute('data-id', this._id);
+		return element;
+	}
+
+  _render(): void {
+    const compileTemplate = this.render();
+    const html = compileTemplate(this.props);
+    const element = this._compileDomElement(html);
+    this._element.insertAdjacentElement('afterbegin', element);
+    this._removeEvents();
+    this._addEvents();
+  }
+
+  _removeEvents(): void {
+		const { events = {} } = this.props;
+		Object.keys(events).forEach(eventName => {
+			this._element.removeEventListener(eventName, events[eventName]);
+		});
+	}
+
+  _addEvents(): void {
+		const { events = {} } = this.props;
+		Object.keys(events).forEach(eventName => {
+			this._element.addEventListener(eventName, events[eventName]);
+		});
+	}
+
+  _compileDomElement(tmpl: string): HTMLElement {
+    const { body } = new DOMParser().parseFromString(tmpl, 'text/html');
+    const element = body?.children[0] as HTMLElement;
+
+    if (this.props.components && !!element) {
+      Object.entries(this.props.components).forEach((item) => {
+        const [ key, instance ] = item;
+        const subComponents = element.querySelectorAll(`${componentAttr}=${key}`);
+        const subComponentsParent = subComponents[0].parentElement;
+
+        if (!subComponents) {
+          return element;
+        }
+
+        if (Array.isArray(instance)) {
+          instance.forEach((instanceEl: Block) => subComponents.forEach(subComponent => 
+            subComponent.insertAdjacentElement('beforebegin', instanceEl.getContent())
+          ));
+        } else {
+          subComponents.forEach(subComponent => subComponent.insertAdjacentElement('beforebegin', instance.getContent()))
+        }
+
+        subComponents.forEach(subComponent => subComponentsParent.removeChild(subComponent));
+      })
+    }
+
+    return element;
+  }
+
+  get element(): HTMLElement {
+		return this._element;
+	}
+
+  getContent(): HTMLElement {
+		return this.element;
+	}
+
+  render(): Function {
+    return () => {};
+  }
+
+  isPropsChanged<T>(oldProps: T, newProps: T): boolean {
+    // TODO: add shallow equal
+    return true;
+  }
+
+  setProps(nextProps: Props): void {
+		if (!nextProps) {
+			return;
+		}
+
+		Object.assign(this.props, nextProps);
+	};
+
+  _makePropsProxy(props: Props): Props {
+    const self = this;
+    const oldProps: Props = JSON.parse(JSON.stringify(props));
+
+    return new Proxy(props, {
+      get(target: Props, prop: string) {
+        const value = target[prop];
+        return typeof value === 'function' ? value.bind(target) : value;
+      },
+      set(target: Props, prop: string, value): boolean {
+        target[prop] = value;
+        self._componentDidUpdate<typeof target>(oldProps, target);
+        return true;
+      },
+      deleteProperty() {
+				throw new Error('нет доступа');
+			},
+    })
+  }
+
+  show(): void {
+		this.getContent().style.display = 'block';
+	}
+
+	hide(): void {
+		this.getContent().style.display = 'none';
+	}
+}
