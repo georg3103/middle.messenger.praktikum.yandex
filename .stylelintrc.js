13c2d01642e87a6ccbb1dee4d875e32590065e19
mkrtchyan_gn <georgiy.mkrtchyan@open.ru>
2022-02-05 00:57:33 +0300

    feat(config): add .stylelintrc.json

--- .stylelintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: [
-    'stylelint-prettier/recommended',
-    'stylelint-config-standard',
-    'stylelint-config-rational-order',
-  ],
-  plugins: ['stylelint-order'],
-  rules: {}
-};
